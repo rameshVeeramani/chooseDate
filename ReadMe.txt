7d1c36f32bd6b400a3faaf66523f693312bce779
rameshVeeramani <Code@123>
2019-02-07 11:24:20 -0600

    first

---
+++ ReadMe.txt
@@ -0,0 +1,25 @@
+
+
+Make sure you have two things
+
+1) From the Date you want the lucky number [OPTIONAL]
+2) Whats your lucky number
+
+ ******** Execute the program  ********
+ ******** Provide the date in format of yyyy-mm-dd  ********
+
+-------Enter date YYY-MM-DD-----------------
+2018-12-01
+
+
+***** Provide your lucky number ********
+2
+
+It would  list all the possible lucky date for you ..
+
+018-12-07 Friday         [3]
+2018-12-16 Sunday         [3]
+2018-12-25 Tuesday        [3]
+2019-01-08 Tuesday        [3]
+
+Best of luck
---
+++ src/main/java/learn/java/App.java
@@ -0,0 +1,76 @@
+package learn.java;
+import java.text.*;
+import java.util.*;
+
+public class App 
+{
+    public static void main( String[] args ) throws ParseException {
+
+        System.out.println(String.format("%n-------Enter date YYY-MM-DD-----------------"));
+        Scanner sc = new Scanner(System.in);
+        String date = sc.nextLine();
+        System.out.println(date);
+        if(date == null || date.isEmpty())
+        {
+            System.out.println("date");
+            date = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
+            System.out.println(date);
+        }
+
+        System.out.println(String.format("%n-------Luck number -----------------"));
+        String lns = sc.nextLine();
+        if ((lns == null ) || lns .isEmpty()){
+            lns ="1,6,7";
+        }
+        String[] x = lns.split(",");
+        ArrayList<Integer> ln = new ArrayList<>();
+        for(String xx : x){
+            int foo;
+            try {
+                foo = Integer.parseInt(xx);
+                ln.add(foo);
+            }
+            catch (NumberFormatException e)
+            {}
+        }
+        System.out.println( "Hello World!" );
+        luckyDate(date,ln);
+    }
+
+    private static void luckyDate(String startDate, ArrayList<Integer> luckynumber) throws ParseException {
+
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+        Date date = format.parse(startDate);
+
+        DateFormat d = new SimpleDateFormat("MM-dd-yyy");
+        System.out.printf("CURRENT DATE - %s %n",date);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int i = 0;
+        while (i < 365) {
+            int month = cal.get(Calendar.MONTH) +1 ;
+            int day = cal.get(Calendar.DATE);
+            int year = cal.get(Calendar.YEAR);
+            int total = month + day + year;
+            int x = findnumber(total);
+            if (luckynumber.contains(x))
+            System.out.printf("%-25s [%d]%n", new SimpleDateFormat("yyyy/MM/dd EEEEE").format(cal.getTime()),x);
+            cal.add(Calendar.DATE, 1);
+            i++;
+        }
+    }
+
+
+    private static int findnumber(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+           // System.out.println(num);
+        }
+
+        sum = (sum < 10) ? sum : findnumber(sum);
+        return sum;
+    }
+}
+
